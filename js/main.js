4d1b6e315f638794e93823ee4ac9affcdf75b15b
Sanesh P S <pssanesh09@gmail.com>
2020-12-01 23:56:02 +0530

    Added all files

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper('.swiper-container', {
+  spaceBetween: 0,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 3000,
+  },
+});
